996ce0a0dd00f43fd1acc67f5606008a4f328237
jan4546 <jhern495@evit.edu>
2025-07-29 12:38:22 -0700

    my first files

---
+++ my-_first_python.py
@@ -0,0 +1,17 @@
+Python 3.13.5 (tags/v3.13.5:6cb20a2, Jun 11 2025, 16:15:46) [MSC v.1943 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> print("Hello Worrld")
+Hello Worrld
+>>> 3+5
+8
+>>> name="Ssagy Noulq"
+>>> primt(name)
+Traceback (most recent call last):
+  File "<pyshell#3>", line 1, in <module>
+    primt(name)
+NameError: name 'primt' is not defined. Did you mean: 'print'?
+>>> print(name)
+Ssagy Noulq
+>>> name=("JashS")
+>>> print(name)
+JashS
---
+++ my_first_python.py
@@ -0,0 +1,3 @@
+##print("Hello World")
+##print("crazy")
+print(3+5)
